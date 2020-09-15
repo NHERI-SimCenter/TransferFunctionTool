eea40d7c50a939f432ea811726cc1b30c0db67c6
Long Chen <longchen@uw.edu>
2020-09-14 19:20:37 -0700

    Update quickstart and index

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,6 +40,7 @@ rst_prolog = """
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinxcontrib.bibtex'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
